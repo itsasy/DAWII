e2e1eba93a537ed9b18314a1727353ac42c58671
Alex <ale.maldo097@gmail.com>
2019-09-12 21:07:12 -0500

    Primer commit

--- Proyecto_Farmacia_2019_SJL_Semana01_por_terminar_2/src/com/farmacia/boleta/RegistraBoletaVenta.java
+++ Proyecto_Farmacia_2019_SJL_Semana01_por_terminar_2/src/com/farmacia/boleta/RegistraBoletaVenta.java
@@ -1,20 +1,55 @@
 package com.farmacia.boleta;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import com.farmacia.entidad.Boleta;
 import com.farmacia.entidad.Cliente;
+import com.farmacia.entidad.DetalleBoleta;
+import com.farmacia.entidad.DetalleBoletaPK;
 import com.farmacia.entidad.Empleado;
+import com.farmacia.modelo.ModeloBoleta;
 
 public class RegistraBoletaVenta {
 
 	public static void main(String[] args) {
-		// Declarar objetos
+		// Declarar objetos para la boleta
 		Boleta bol = new Boleta();
+
 		Cliente cli = new Cliente();
 		cli.setCodCliente(2);
 		bol.setClienteBoleta(cli);
+
 		Empleado emp = new Empleado();
 		emp.setCodEmpleado(1);
+
 		bol.setEmpleadoBoleta(emp);
+		bol.setFechaEmision(new Date());
+		bol.setMontoBoleta(500);
+
+		// Enviamos los datos al detalle
+		List<DetalleBoleta> listarDatos = new ArrayList<DetalleBoleta>();
+
+		DetalleBoleta detabol = new DetalleBoleta();
+		DetalleBoletaPK detabolPK = new DetalleBoletaPK();
+
+		// Enviando los datos
+		detabol.setCantidad(3); // Cantidad del medicamento
+		detabol.setPrecio(11.11); // Precio del medicamento
+		detabolPK.setCodMedicamento(1); // Medicamento
+
+		// Enviando los datos del detalle
+		detabol.setPk(detabolPK);
+
+		// Agregando los datos a la colección
+		listarDatos.add(detabol);
+		bol.setListadetalleBoleta(listarDatos);
+
+		// llamando al modelo
+		new ModeloBoleta().RegistraBoletaVenta(bol);
+		System.out.println("Registrado correctamente");
+
 	}
 
 }
